2b946534f10398535a88547323b711e919cc6db9
unknown <sahribesh0123@gmail.com>
2024-09-30 20:13:49 +0545

    RandomColor

--- App.tsx
+++ App.tsx
@@ -1,117 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+//  const backgroundColor = '#000000';
+ const [backgroundColor,setbackgroundColor] = useState('ffffff');
+ const colorCustom = '#' + backgroundColor;
+  function generateColor(){
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    console.log(randomColor);
+    setbackgroundColor(randomColor);
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+      <>
       <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      backgroundColor={colorCustom} />
+      <View style={[styles.sectionContainer,{backgroundColor:colorCustom}]}>
+        <TouchableOpacity onPress={()=>{
+          // setbackgroundColor('000000');
+          generateColor();
+        }}>
+          <View style={{
+            alignItems:'center',
+            justifyContent:'center',
+            // height:35,
+            padding: 12,
+            borderRadius : 8,
+            backgroundColor : 'aqua',
+            }}>
+            <Text selectable={true} style={{fontSize:20,fontWeight:'bold'}}>Color : {colorCustom}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      </>
   );
 }
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    flex: 1,
+    alignItems:'center',
+    justifyContent : 'center',
+    // backgroundColor:'red',
   },
 });
 
